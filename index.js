4ecd5e1fc376266dc366d3204599fa70fc0dfd07
Vinayak <svinayak994@gmail.com>
2023-03-19 02:24:22 +0530

    improved code

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-const API = "52f1706b834fb3536ea92951ec7ee5f0";
-const cloud=document.getElementById('cloud');
-const temp=document.getElementById('temp');
-const wind=document.getElementById('wind');
-const icon=document.getElementById('icon');
-const name=document.getElementById('name');
+const API_KEY = "YOUR_API_KEY";
+const searchButton = document.getElementById("search-button");
+const cityInput = document.getElementById("city-input");
+const weatherCity = document.getElementById("weather-city");
+const weatherIcon = document.getElementById("weather-icon");
+const weatherTemp = document.getElementById("weather-temp");
+const weatherFeelsLike = document.getElementById("weather-feels-like");
+const weatherHumidity = document.getElementById("weather-humidity");
+const weatherWindSpeed = document.getElementById("weather-wind-speed");
+const weatherDescription = document.getElementById("weather-description");
 
-const submit = document.getElementById("submit");
-submit.addEventListener("click", (e) => {
-  e.preventDefault();
-  const city = document.getElementById("city").value;
-  name.innerHTML=city;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}&units=metric`;
+searchButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const city = cityInput.value.trim();
+  if (!city) {
+    alert("Please enter a city name.");
+    return;
+  }
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
   fetch(url)
-    .then((response) => response.json())
-    .then((result) => {
-      temp.innerHTML=result["main"]["temp"]+" &#176C";
-      cloud.innerHTML=result["weather"][0]["description"];
-      wind.innerHTML="Wind Speed: "+result["wind"]["speed"]+" m/sec";
-      let icon_id=result["weather"][0]["icon"];
-      // http://openweathermap.org/img/w/10d.png
-      const icon_url=`http://openweathermap.org/img/w/${icon_id}.png`;
-      icon.src=icon_url;
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
     })
-    .catch((err) => {
-      console.log(err);
+    .then((data) => {
+      console.log(data);
+      weatherCity.textContent = data.name;
+      weatherTemp.textContent = `${data.main.temp.toFixed(1)} °C`;
+      weatherFeelsLike.textContent = `${data.main.feels_like.toFixed(1)} °C`;
+      weatherHumidity.textContent = `${data.main.humidity}%`;
+      weatherWindSpeed.textContent = `${data.wind.speed.toFixed(1)} m/s`;
+      weatherDescription.textContent = data.weather[0].description;
+      weatherIcon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+      weatherIcon.alt = data.weather[0].description;
+    })
+    .catch((error) => {
+      console.error("Error fetching weather data:", error);
+      alert("Failed to fetch weather data. Please try again later.");
     });
 });
